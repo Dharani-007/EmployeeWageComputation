f22b210184c9c6e46bbccfa9a669f765a1ead51f
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-02 23:40:39 +0530

    Added : Switch Case

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,36 +1,43 @@
 import java.util.Random;
-public class EmployeeWage {
-
-    public static final int IS_PRESENT = 1;
+public class EmployeeWage{
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static int empHrs = 0;
 
-     // Method for computing the wage for employee.
+     //Method for computing the wage for employee.
 
     private static void wageComputation() {
         Random random = new Random();
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT) {
-            System.out.println("Employee is Present ");
-            empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            if (empCheck == IS_FULL_TIME) {
-                System.out.println("Employee is Present Full Time");
-                empHrs = 8;
-            } else if (empCheck == IS_PART_TIME) {
-                System.out.println("Employee is Present Part Time ");
-                empHrs = 4;
-            } else {
-                System.out.println("Employee is Absent ");
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present Full Time");
+            empHrs = 8;
+        } else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Present Part Time ");
+            empHrs = 4;
+        } else {
+            System.out.println("Employee is Absent ");
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present Full Time");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Present Part Time ");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent ");
             }
+
             int empWage = empHrs * EMP_RATE_PER_HOUR;
             System.out.println("Employee Wage is : " + empWage);
         }
     }
-     //     main method-starting point of EmpWageComputation program.
+         // main method - starting point of EmpWageComputation program.
 
-        public static void main (String[]args){
+        public static void main(String[] args) {
             System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
             wageComputation();
         }
