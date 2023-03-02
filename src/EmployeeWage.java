16440477e9c0ce8d1d64490865fb8f052b18d48e
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-02 23:58:47 +0530

    Added : Class Method to Compute Employee Wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,43 +1,42 @@
 import java.util.Random;
+
 public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int MAX_HRS_IN_MONTHS = 100;
 
-    public static int empHrs = 0;
-    public static int totalEmpHrs = 0;
-    public static int totalWorkingDays = 0;
+    //Class Variables
+    public static int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
     public static int numOfWorkingDays = 20;
 
-   // Method for computing the wage for employee.
+
+    // Method for computing the wage for employee.
+    // Class method for computing the wage for employee.
 
     private static void wageComputation() {
         Random random = new Random();
-        for (int i = 0; i < numOfWorkingDays; i++) {
-            while (totalEmpHrs < MAX_HRS_IN_MONTHS && totalWorkingDays < numOfWorkingDays) {
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                switch (empCheck) {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                }
-                totalEmpHrs = totalEmpHrs + empHrs;
+        while (totalEmpHrs < MAX_HRS_IN_MONTHS && totalWorkingDays < numOfWorkingDays) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
             }
-            int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Employee Wage is : " + empWage);
+            totalEmpHrs = totalEmpHrs + empHrs;
         }
+        int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage is : " + empWage);
     }
 
-         // main method - starting point of EmployeeWageComputation program.
+    //  main method - starting point of EmpWageComputation program.
 
-
-        public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-            wageComputation();
-        }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        wageComputation();
     }
+}
