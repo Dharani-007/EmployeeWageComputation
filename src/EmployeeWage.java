107d88e8908b4d998b06822df029148cb3592747
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-02 23:35:21 +0530

    Added : Add Part Time

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,27 +1,37 @@
 import java.util.Random;
-public class EmployeeWage{
+public class EmployeeWage {
+
     public static final int IS_PRESENT = 1;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static int empHrs = 0;
-//Employee is present and absent
+
+     // Method for computing the wage for employee.
+
     private static void wageComputation() {
         Random random = new Random();
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_PRESENT) {
             System.out.println("Employee is Present ");
-            empHrs = 8;
-        } else {
-            System.out.println("Employee is Absent ");
+            empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            if (empCheck == IS_FULL_TIME) {
+                System.out.println("Employee is Present Full Time");
+                empHrs = 8;
+            } else if (empCheck == IS_PART_TIME) {
+                System.out.println("Employee is Present Part Time ");
+                empHrs = 4;
+            } else {
+                System.out.println("Employee is Absent ");
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Employee Wage is : " + empWage);
         }
+    }
+     //     main method-starting point of EmpWageComputation program.
 
-        int empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage is : " + empWage);
+        public static void main (String[]args){
+            System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+            wageComputation();
+        }
     }
-    /**
-     main method - starting point of EmpWageComputation program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        wageComputation();
-    }
-}
