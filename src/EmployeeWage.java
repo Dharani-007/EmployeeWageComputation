02dda4d0a72559eeaa32a17378ada54495465d99
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-03 22:48:34 +0530

    Added : Employee Wage for multiple companies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,42 +1,37 @@
 import java.util.Random;
+
 public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int MAX_HRS_IN_MONTHS = 100;
-
-    //Class Variables
-    public static int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
-    public static int numOfWorkingDays = 20;
 
 
     // Method for computing the wage for employee.
     // Class method for computing the wage for employee.
 
-    private static void wageComputation() {
+    private static void wageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMont) {
+        int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
         Random random = new Random();
-        while (totalEmpHrs < MAX_HRS_IN_MONTHS && totalWorkingDays < numOfWorkingDays) {
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-            }
-            totalEmpHrs = totalEmpHrs + empHrs;
-        }
-        int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage is : " + empWage);
-    }
+        while (totalEmpHrs < maxHoursPerMont && totalWorkingDays < numOfWorkingDays) {
+            while (totalEmpHrs < maxHoursPerMont && totalWorkingDays < numOfWorkingDays) {
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empCheck) {
+                    case IS_FULL_TIME:
 
+                }
+
+                int empWage = totalEmpHrs * empRatePerHour;
+                System.out.println("Employee Wage for company " + company + " is : " + empWage);
+                System.out.println("Employee Wage for company " + company + " is : " + empWage);
+            }
+        }
+    }
     //  main method - starting point of EmpWageComputation program.
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        wageComputation();
+        wageComputation("Reliance", 20, 2, 10);
+        wageComputation("Bsnl", 30, 4, 14);
     }
 
 }
